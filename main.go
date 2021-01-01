8bf3ed59d034a45e5796356b7d255bf04cfe3110
Wang Tao <wangtaobj@fenbi.com>
2021-01-01 17:16:36 +0800

    init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/sevge/go-gin-example/routers"
+    "github.com/sevge/go-gin-example/pkg/setting"
+)
+
+func main() {
+    router := routers.InitRouter()
+
+    s := &http.Server{
+        Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+        Handler:        router,
+        ReadTimeout:    setting.ReadTimeout,
+        WriteTimeout:   setting.WriteTimeout,
+        MaxHeaderBytes: 1 << 20,
+    }
+
+    s.ListenAndServe()
+}
